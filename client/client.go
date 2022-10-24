da355b9309ee567f5a2c665ffe428e4f9a0eaa17
Gvidas  Razevicius <gvidas.razevicius@couchbase.com>
2022-10-24 16:55:53 +0100

    Added CLI user creation. Improved CLI information.

--- client/client.go
+++ client/client.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 
 	server "github.com/gvidas-razevicius/rest-api-task/server"
 	cobra "github.com/spf13/cobra"
@@ -22,10 +23,17 @@ const (
 var rootCmd = &cobra.Command{}
 
 var getAgeCmd = &cobra.Command{
-	Use:   "getage",
+	Use:   "get-age <name>",
 	Short: "Gets the age of a person by name",
 	Args:  cobra.ExactArgs(1),
 	Run:   getAge,
+}
+
+var createUserCmd = &cobra.Command{
+	Use:   "cr-user <name> <age>",
+	Short: "Creates user in server",
+	Args:  cobra.ExactArgs(2),
+	Run:   createUser,
 }
 
 // Builds and performs a request given parameters.
@@ -56,7 +64,7 @@ func makeRequest(method string, endpoint string, values url.Values, json []byte)
 	return http.DefaultClient.Do(req)
 }
 
-// Gets run for the getage command and returns the age of the given persons name
+// Gets run for the get-age command and returns the age of the given persons name
 func getAge(cmd *cobra.Command, args []string) {
 	resp, err := makeRequest(http.MethodGet, args[0], nil, nil)
 	if err != nil {
@@ -66,7 +74,7 @@ func getAge(cmd *cobra.Command, args []string) {
 
 	if resp.StatusCode == http.StatusOK {
 		var res server.User
-		if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		if err := json.NewDecoder(resp.Body).Decode(&res); err != nil && err != io.EOF {
 			log.Fatalf("Could not decode results into json: %v", err)
 		}
 
@@ -77,8 +85,47 @@ func getAge(cmd *cobra.Command, args []string) {
 	}
 }
 
+// Gets run for the cr-user command and returns the age of the given persons name
+func createUser(cmd *cobra.Command, args []string) {
+	// Convert the string argument into int
+	age, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Fatalf("Age argument is not a valid number: %v", err)
+	}
+	// Form the json struct
+	user := server.User{
+		Name: args[0],
+		Age:  server.StringInt(age),
+	}
+
+	// Marshal data struct
+	userBytes, err := json.Marshal(user)
+	if err != nil && err != io.EOF {
+		log.Fatalf("Could not encode data: %v", err)
+	}
+
+	resp, err := makeRequest(http.MethodPost, "", nil, userBytes)
+	if err != nil {
+		log.Fatalf("Could not make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var res server.User
+		if err := json.NewDecoder(resp.Body).Decode(&res); err != nil && err != io.EOF {
+			log.Fatalf("Could not decode results into json: %v", err)
+		}
+
+		fmt.Println("User created!")
+	} else {
+		fmt.Println("Could not create user! Server responded with: ")
+		fmt.Println(resp.Status, resp.Header)
+	}
+}
+
 func main() {
 	rootCmd.AddCommand(getAgeCmd)
+	rootCmd.AddCommand(createUserCmd)
 	if err := getAgeCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
 		os.Exit(1)
