5a5f0f387ed4e50b8a574d098c59f546c673d773
Gvidas  Razevicius <gvidas.razevicius@couchbase.com>
2022-10-24 16:13:58 +0100

    Added universal request making. Added comments. Improved error handling and messages.

--- client/client.go
+++ client/client.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	server "github.com/gvidas-razevicius/rest-api-task/server"
 	cobra "github.com/spf13/cobra"
 )
 
-const endpoint string = "http://localhost:8080"
+const (
+	APIroot  = "http://localhost:8080"
+	APIusers = APIroot + "/users"
+)
 
 var rootCmd = &cobra.Command{}
 
@@ -21,20 +28,53 @@ var getAgeCmd = &cobra.Command{
 	Run:   getAge,
 }
 
-// TODO: implement proper query builder
-func getAge(cmd *cobra.Command, args []string) {
-	r, err := http.Get(endpoint + "/users/" + args[0])
-	if err != nil {
-		fmt.Println("Error getting the age!", err)
-		return
+// Builds and performs a request given parameters.
+func makeRequest(method string, endpoint string, values url.Values, json []byte) (*http.Response, error) {
+	// Form body for request
+	var body io.Reader
+	if json != nil {
+		body = bytes.NewBuffer(json)
 	}
-	var res server.User
-	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
-		fmt.Println("Error decoding results!", err)
-		return
+	req, err := http.NewRequest(method, APIusers, body)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Printf("%s age is %d\n", res.Name, res.Age)
+	// Set content type if body exists
+	if json != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	// Add values if any
+	if values != nil {
+		req.URL.RawQuery = values.Encode()
+	}
+
+	// Add additional path to URL
+	req.URL = req.URL.JoinPath(endpoint)
+
+	return http.DefaultClient.Do(req)
+}
+
+// Gets run for the getage command and returns the age of the given persons name
+func getAge(cmd *cobra.Command, args []string) {
+	resp, err := makeRequest(http.MethodGet, args[0], nil, nil)
+	if err != nil {
+		log.Fatalf("Could not make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var res server.User
+		if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+			log.Fatalf("Could not decode results into json: %v", err)
+		}
+
+		fmt.Printf("%s is %d years old\n", res.Name, res.Age)
+	} else {
+		fmt.Println("Could not get results! Server responded with: ")
+		fmt.Println(resp.Status, resp.Header)
+	}
 }
 
 func main() {
